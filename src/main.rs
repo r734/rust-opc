9e6707e8ea691f918954aaae88ba48186e944993
Jonathan Roeber <r734@fastmail.com>
2019-07-24 19:55:03 -0400

    apply a color to all pixels

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,25 @@ fn main() {
     let num_pixels = 240;
     let packet_size = 4 + (num_pixels * 3);
 
-    let mut vec: Vec<u8> = vec![0u8; packet_size];
+    let mut payload: Vec<u8> = vec![0u8; packet_size];
 
-    vec[0] = 0x01; // Channel 1
-    vec[1] = 0x00; // Command 0 (set 8-bit pixel colors)
+    payload[0] = 0x01; // Channel 1
+    payload[1] = 0x00; // Command 0 (set 8-bit pixel colors)
     
     let len: u16 = (num_pixels * 3).try_into().unwrap();
-    vec[2] = (len >> 8) as u8;
-    vec[3] = len as u8;
+    payload[2] = (len >> 8) as u8;
+    payload[3] = len as u8;
 
-    for i in 4..packet_size {
-        vec[i] = 0x01;
-    }
-
-    let my_rgb: LinSrgb = Hsv::new(66., 1., 0.1).into(); // see https://stackoverflow.com/a/12894053 for Srgb vs LinSrgb
+    let my_rgb: LinSrgb = Hsv::new(155., 1., 0.01).into(); // see https://stackoverflow.com/a/12894053 for Srgb vs LinSrgb
     let my_u8_rgb: LinSrgb<u8> = my_rgb.into_format();
 
-    vec[4] = my_u8_rgb.red;
-    vec[5] = my_u8_rgb.green;
-    vec[6] = my_u8_rgb.blue;
+    for i in (4..packet_size-2).step_by(3) {
+        payload[i] = my_u8_rgb.red;
+        payload[i+1] = my_u8_rgb.green;
+        payload[i+2] = my_u8_rgb.blue;
+    }
 
     let socket = UdpSocket::bind("0.0.0.0:34567").unwrap();
 
-    socket.send_to(&vec, "192.168.0.53:5000").unwrap();
+    socket.send_to(&payload, "192.168.0.53:5000").unwrap();
 }
